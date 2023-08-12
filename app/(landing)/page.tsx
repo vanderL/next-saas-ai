0d34e9a752f60c6eb66dcf8377cd5e5cf3445833
Vander Lima <vnd.vander@gmail.com>
2023-08-12 16:53:31 -0300

    feat: auth clerk

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <div>
       Landing Page (Unprotected)
+      <div>
+        <Link href={"/sing-in"}>
+          <Button>
+            Login
+          </Button>
+        </Link>
+
+        <Link href={"/sign-up"}>
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }

c21921d0d7e6b0f928920f9dc462d42259fa7090
Vander Lima <vnd.vander@gmail.com>
2023-08-13 20:31:56 -0300

    feat: Conversation AI Ui

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -10,35 +10,35 @@ const tools = [
     icon: ChatBubbleIcon,
     href: "/conversation",
     color: "text-violet-500",
-    bgColor: "bg-violet-500/10"
+    bgColor: "bg-violet-500/25"
   },
   {
     label: "Image Generation",
     icon: ImageIcon,
     href: "/image",
     color: "text-pink-700",
-    bgColor: "bg-pink-700/10"
+    bgColor: "bg-pink-700/25"
   },
   {
     label: "Video Generation",
     icon: VideoIcon,
     href: "/video",
     color: "text-orange-700",
-    bgColor: "bg-orange-700/10"
+    bgColor: "bg-orange-700/25"
   },
   {
     label: "Music Generate",
     icon: SpeakerLoudIcon,
     href: "/music",
     color: "text-emerald-500",
-    bgColor: "bg-emerald-500/10"
+    bgColor: "bg-emerald-500/25"
   },
   {
     label: "Code Generate",
     icon: CodeIcon,
     href: "/code",
     color: "text-green-700",
-    bgColor: "bg-green-700/10"
+    bgColor: "bg-green-700/25"
   },
 ]
 
